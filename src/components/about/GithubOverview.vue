d42f602ad62d11faad35df1ca90372bc29ef728d
Violeta Millan <20193tn066@utez.edu.mx>
2023-01-10 20:20:31 -0600

    refactor: Modified cover structure
    Changed cover components

---
+++ src/components/about/GithubOverview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="center-item mb-1">
+      <vs-avatar circle color="#434868" size="30">
+        <i class='bx bxl-github'></i>
+      </vs-avatar>
+      &emsp;Github overview
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'GithubOverview',
+}
+</script>
+
+<style></style>
