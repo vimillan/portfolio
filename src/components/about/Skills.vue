068eab460661b3ec53dc7296a12a70c618704adc
Violeta Millan <20193tn066@utez.edu.mx>
2023-01-14 14:30:47 -0600

    style: Modified animation for progressbars

--- src/components/about/Skills.vue
+++ src/components/about/Skills.vue
@@ -13,6 +13,19 @@
         <div class="progress-bar progress-moved bg-dark rounded" style="--percentage: 10%;"></div>
       </div>
     </div>
+    <div
+      v-for="(item, i) in $vs.getPage(list, page, max)"
+      :key="i"
+      class="mb-2"
+    >
+      <vs-row justify="space-between">
+        <vs-col lg="1" class="text-w-bold">Title</vs-col>
+        <vs-col lg="1" class="text-center">20%</vs-col>
+      </vs-row>
+      <div class="progress rounded" role="progressbar">
+        <div class="progress-bar progress-moved bg-dark rounded" style="--percentage: 40%;"></div>
+      </div>
+    </div>
     <vs-pagination
       buttons-dotted
       v-model="page"
@@ -28,7 +41,7 @@ export default {
   data: () => ({
     page: 1,
     max: 4,
-    list: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    list: [1, 2],
   }),
 }
 </script>
