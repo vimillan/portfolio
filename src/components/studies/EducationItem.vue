8567f912c398653b48ca64cd4c9edfe3776f59b4
Violeta Millan <20193tn066@utez.edu.mx>
2023-01-16 22:02:07 -0600

    feat: Added education section

---
+++ src/components/studies/EducationItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <vs-row class="rounded shadow bg-white pa-2 mb-3">
+    <vs-col lg="2" md="12" sm="12" xl="12" class="text-center">
+      <div>
+        <div class="text-w-bold">Aug</div>
+        <div>2021-2022</div>
+      </div>
+    </vs-col>
+    <vs-col lg="10" md="12" sm="12" xl="12">
+      <div class="text-w-bold c-primary">
+        Universidad Tecnológica Emiliano Zapata
+      </div>
+      <span class="text-w-bold fs-7">
+        Computer Software Engineering
+      </span>
+      <div class="mt-1 fs-7">
+        My career is focused on software development and management, both
+        practically and theoretically, using agile methodologies such as scrum.
+        I have my TSU degree and I am a software engineer
+      </div>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script>
+export default {
+  name: 'EducationItem',
+}
+</script>
+
+<style></style>
---
+++ src/components/studies/Studies.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="px-4 bg-blue-back mt-4">
+    <div class="text-w-bold fs-3">Education</div>
+    <vs-row justify="space-between">
+      <vs-col lg="4" md="12" sm="12" xl="12">
+        <div class="pa-3 text-center bg-white rounded shadow mt-4 mb-3">
+          I have developed web applications with vue 2 and react js, I have made
+          rest apis in spring boot and php, in addition to making mobile apps
+          from android studio, managing cloud services and using the git flow
+          methodology
+        </div>
+      </vs-col>
+      <vs-col lg="7" md="12" sm="12" xl="12">
+        <div v-for="(i, item) in 2" :key="i">
+          <EducationItem v-if="item != null" />
+        </div>
+      </vs-col>
+    </vs-row>
+  </div>
+</template>
+
+<script>
+import EducationItem from './EducationItem.vue'
+export default {
+  components: { EducationItem },
+  name: 'Studies',
+}
+</script>
+
+EducationItem
+<style></style>
